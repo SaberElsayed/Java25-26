a890c13fb92e5a80dc666a4252d3d61b9efd70ff
Saber <s.elsayed@sharjah.ac.ae>
2026-01-20 14:20:00 +0400

    Lec1-Week2

--- Week2-Lec1/src/Main.java
+++ Week2-Lec1/src/Main.java
@@ -1,50 +1,51 @@
 /**
  * This class is for week 2, lecture 1
- * it is about String and other stuff
+ * it is about the String class, double/float and logical errors
  *
  * @author Saber Elsayed
  * @version 1.1
- * @date:
+ * @since: 20/01/2026
  */
 public class Main {
     /**
-     * this is the main function
+     * this is the main method to run
      *
-     * @param args
+     * @param args input parameters
      */
     public static void main(String[] args) {
         double a = 0.555555176876871;
-        System.out.println((float) a);
+        System.out.println((float) a); // cast double to float
 
-        float b = 0.555555176876871f;
+        float b = 0.555555176876871f; // we need to use f to define a variable of type float
         System.out.println(b);
 
         String text = "Hello EveryONE";
-        // get length
-        System.out.println(text.length());
+
+        System.out.println(text.length());// get the length of the string
         System.out.println(text.charAt(0)); // returns char at location 0
         System.out.println(text.contains("E")); // returns T/F if string exists
-        System.out.println(text.contains("Saber"));
-        System.out.println(text.toLowerCase());
-        System.out.println(text.toUpperCase());
+        System.out.println(text.contains("Saber"));// check if the text contains "Saber"
+        System.out.println(text.toLowerCase());// convert it lower case
+        System.out.println(text.toUpperCase()); // convert the text to uppercase
         System.out.println(text.replace(" ", "!!!"));
         System.out.println(text.replace("e", "yyyyyyyyy"));
         System.out.println(text.equals("Hello everyONE"));
 
-//        text= text.replace("e", "yyyyyyyyy");
+
+        System.out.println(text.isEmpty()); // check if the text is empty
+        System.out.println(text.equalsIgnoreCase("Hello everyONE")); // compare two text ignoring the case
+        System.out.println(text.substring(1, 5)); // take a portion/subset of the text from index 0 to index 5
+
+
+        text = text.replace("e", "yyyyyyyyy"); // this will update the original text
         System.out.println(text);
 
-        System.out.println(text.isEmpty());
-        System.out.println(text.equalsIgnoreCase("Hello everyONE")); // compare two text ignoring the case
-        System.out.println(text.substring(1, 5));
-
-        System.out.println((8 / 2) + 7 * 2 - (10 / 2) + (8 % 3));
+        System.out.println((8 / 2) + 7 * 2 - (10 / 2) + (8 % 3));// expected outcome is 15
 
 
-        System.out.println("Saber");
         //System.out.println(1/0); // run time error
-        int cc =32;
-        System.out.println(cc);
+        int cc = 032;
+        System.out.println(cc); // this will print the number in octal system, as the number starts with 0
     }
 
 }
